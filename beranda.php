bc6d2d687c6d5c26a8fd95729cc4afaebe475102
Swag-iwnl-m1 <89894873+Swag-iwnl-m1@users.noreply.github.com>
2025-04-25 19:43:54 +0700

    pembaruan

--- beranda.php
+++ beranda.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beranda</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <img src="./images/monyet.png" alt="gambar monyet">
-Ini adalah beranda    
+<body class="beranda">
+    <img class="Monyet" src="./images/monyet.png" alt="gambar monyet">
+    <p class="beranda"> Halo semua aku adalah mangky</p>
+  
 </body>
 </html>
